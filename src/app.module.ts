47a4260d19aba9a370d6802d004a2792395e9a7a
Cleiton <cleiton.queiroz@proton.me>
2024-07-05 02:11:59 -0300

    update: update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { UsersModule } from './users/users.module';
 import { ChatsModule } from './chats/chats.module';
 import { UsersPreferencesModule } from './users-preferences/users-preferences.module';
 import { PrismaService } from './prisma/prisma.service';
+import { MessagesModule } from './messages/messages.module';
+import { ProvidersModule } from './providers/providers.module';
 
 @Module({
   imports: [
     UsersModule,
     ChatsModule,
     UsersPreferencesModule,
+    MessagesModule,
+    UsersPreferencesModule,
+    ProvidersModule,
   ],
   providers: [PrismaService],
 })
