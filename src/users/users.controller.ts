f48d330a454e4d7b265b49fb0c21b3315e1f11df
Cleiton <cleiton.queiroz@proton.me>
2024-07-06 18:12:25 -0300

    feat: add exeption handle

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, Delete, UseGuards, Req, Res } from '@nestjs/common';
+import { Controller, Get, Param, Delete, UseGuards, Req, Res, Logger, HttpException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { UserEntity } from './entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
@@ -14,6 +14,7 @@ export class UsersController {
     private readonly usersService: UsersService,
     private readonly authService: AuthService
   ) { }
+  private readonly logger = new Logger(UsersController.name);
 
   @Get('auth/google')
   @UseGuards(AuthGuard('google'))
@@ -21,6 +22,7 @@ export class UsersController {
 
   @Get('oauth2/redirect/google')
   @UseGuards(AuthGuard('google'))
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async googleAuthRedirect(@Req() req, @Res() res) {
     try {
       const user = await this.authService.authenticateWithGoogle(req);
@@ -32,13 +34,31 @@ export class UsersController {
 
   @Get(':id')
   @ApiOkResponse({ type: UserEntity })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async getUserById(@Param('id') id: string): Promise<User> {
-    return this.usersService.getUserById(id);
+    try {
+      const user = await this.usersService.getUserById(id);
+      if (!user) throw new Error('User not found');
+      return user;
+    } catch (error) {
+      this.logger.error(error);
+      if (error.message === 'User not found') throw new HttpException('User not found', 404);
+      throw new HttpException('Internal server error', 500, { cause: new Error()});
+    }
   }
 
   @Delete(':id')
   @ApiOkResponse()
-  remove(@Param('id') id: string) {
-    return this.usersService.deleteUser(id);
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  async remove(@Param('id') id: string, @Res() res): Promise<void> {
+    try {
+      return await this.usersService.deleteUser(id);
+    } catch (error) {
+      this.logger.error(error);
+      if (error.message === 'User not found') throw new HttpException('User not found', 404);
+      throw new HttpException('Internal server error', 500, { cause: new Error()});
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -6,73 +6,62 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) { }
-  private readonly logger = new Logger(UsersService.name);
 
   async getUserById(id: string): Promise<User | null> {
-    try {
-      return await this.prisma.user.findUnique({
-        where: { id },
-        include: {
-          chats: true,
-          preferences: true
-        }
-      });
-    } catch (error) {
-      this.logger.error(error);
-    }
+    return await this.prisma.user.findUnique({
+      where: { id },
+      include: {
+        chats: true,
+        preferences: true
+      }
+    });
   }
 
   async createUser(data: CreateUserDto): Promise<User> {
-    try {
-      await this.prisma.user.create({
-        data,
-      });
-      await this.prisma.preferences.create({
-        data: {
-          userId: data.id,
-        }
-      });
+    await this.prisma.user.create({
+      data,
+    });
 
-      return await this.prisma.user.findUnique({
-        where: { id: data.id },
-        include: {
-          preferences: true
-        }
-      });
-    } catch (error) {
-      this.logger.error(error);
-    }
+    await this.prisma.preferences.create({
+      data: {
+        userId: data.id,
+      }
+    });
+
+    return await this.prisma.user.findUnique({
+      where: { id: data.id },
+    });
   }
 
   async deleteUser(id: string): Promise<void> {
-    try {
-      const chats = await this.prisma.chat.findMany({
-        where: { userId: id },
-      });
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    
+    if(!user) throw new Error('User not found');
 
-      chats.map(async (chat) => {
-        await this.prisma.$transaction([
-          this.prisma.message.deleteMany({
-            where: { chatId: chat.id },
-          }),
-          this.prisma.chat.delete({
-            where: { id: chat.id },
-          }),
-        ]);
-      });
+    const chats = await this.prisma.chat.findMany({
+      where: { userId: id },
+    });
 
+    chats.map(async (chat) => {
       await this.prisma.$transaction([
-        this.prisma.preferences.delete({
-          where: { userId: id },
+        this.prisma.message.deleteMany({
+          where: { chatId: chat.id },
         }),
-        this.prisma.user.delete({
-          where: { id },
-        })
+        this.prisma.chat.delete({
+          where: { id: chat.id },
+        }),
       ]);
-    }
-    catch (error) {
-      this.logger.error(error);
-    }
-  }
+    });
 
+    await this.prisma.$transaction([
+      this.prisma.preferences.delete({
+        where: { userId: id },
+      }),
+      this.prisma.user.delete({
+        where: { id },
+      })
+    ]);
+  }
 }
