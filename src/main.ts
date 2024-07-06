b3ec16442d88c84f12766e73794f149cb2df5f68
Cleiton <cleiton.queiroz@proton.me>
2024-07-06 20:09:56 -0300

    feat: add cors configuration object

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    }
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Next-Ai')
