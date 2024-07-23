a5b279cdbe3bcb8b99e389b2d7a24f1df13d7302
Cleiton <cleiton.queiroz@proton.me>
2024-07-23 20:36:49 -0300

    fix: fix proxy issue on prod

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       origin: process.env.CORS_ORIGIN,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
     },
   });
+
+  app.set('trust proxy', 1);
 
   const config = new DocumentBuilder()
     .setTitle('Next-Ai')
